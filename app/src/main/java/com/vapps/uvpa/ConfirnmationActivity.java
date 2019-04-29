944f8c573be841a26b1525a06ea3735b180ed3e7
vanik-jain <jainvanik100@gmail.com>
2019-04-29 08:50:18 +0530

    Login configration for rails

---
+++ app/src/main/java/com/vapps/uvpa/ConfirnmationActivity.java
@@ -0,0 +1,17 @@
+package com.vapps.uvpa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ConfirnmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirnmation);
+    }
+
+    public void nextActivity(View view) {
+    }
+}
--- app/src/main/java/com/vapps/uvpa/LoginActivity.java
+++ app/src/main/java/com/vapps/uvpa/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.vapps.uvpa;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
@@ -18,61 +19,191 @@ import com.parse.ParseException;
 
 import com.parse.ParseUser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
 
 
 public class LoginActivity extends AppCompatActivity
 {
 
-    EditText username;
-    EditText password;
+    private EditText mUserEmail;
+    private EditText mUserPassword;
+    int resp;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
 //        getSupportActionBar().hide();
-
-
-
-
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-
+        mUserEmail = findViewById(R.id.username);
+        mUserPassword = findViewById(R.id.password);
 
 
     }
+
+    //---------------------------Sign Up-------------------------------------//
 
     public void SignUp(View view)
     {
-        startActivity(new Intent(LoginActivity.this,SignUp.class));
+        startActivity(new Intent(LoginActivity.this, SignUp.class));
 
     }
 
+
+    //---------------------------Login-------------------------------------//
 
     public void login(View view)
     {
 
-        ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback()
-        {
-            @Override
-            public void done(ParseUser user, ParseException e) {
+        JSONObject loginDetails = new JSONObject();
+        JSONObject holder = new JSONObject();
 
-                if (user != null)
-                {
-                    Toast.makeText(LoginActivity.this,"Login succesful!",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(LoginActivity.this,RepairOrder1.class));
-                }
-                else
-                {
-                    Toast.makeText(LoginActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
-                }
+        try {
+
+            if (!(mUserEmail.getText().toString().equals("") || mUserPassword.getText().toString().equals("")))
+
+            { loginDetails.put("email", mUserEmail.getText().toString());
+                loginDetails.put("password", mUserPassword.getText().toString());
+                holder.put("user", loginDetails);
+
+                CredentialsVerifier task = new CredentialsVerifier();
+                task.execute("http://www.amxdp.fun/api/sessions", holder.toString());
+
             }
-
-
-        });
-
+            else
+                {
+                    Toast.makeText(LoginActivity.this, "Login or Password can't be blank!", Toast.LENGTH_SHORT).show();
+                 }
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
     }
+
+
+    //---------------------------AsyncTask-------------------------------------//
+
+
+    class CredentialsVerifier extends AsyncTask<String,Void,String>
+    {
+
+        @Override
+        protected String doInBackground(String... params)
+        {
+
+            try {
+                URL url = new URL(params[0]);
+
+                HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+                connection.addRequestProperty("Accept","application/json");
+                connection.addRequestProperty("Content-Type","application/json");
+
+                connection.setRequestMethod("POST");
+
+                connection.setDoOutput(true);
+                connection.connect();
+                DataOutputStream outputStream=new DataOutputStream(connection.getOutputStream());
+                outputStream.writeBytes(params[1]);
+                Log.i("LOGIN JSON DATA",params[1]);
+                resp=connection.getResponseCode();
+                String response=getResponse(connection);
+                 Log.i("RESPONSE CODE",String.valueOf(resp));
+                 Log.i("RESPONSE",response);
+                if(resp!=200)
+                {
+                    Log.i("LOGIN RESPONSE CODE",String.valueOf(resp));
+                    return String.valueOf(resp);
+                }
+
+                return response;
+            }
+            catch (ProtocolException e)
+            {
+                e.printStackTrace();
+            }
+
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String response)
+        {
+            //progressBar.setVisibility(View.INVISIBLE);
+
+            if (response != null)
+            {
+                if(response.equals("401"))
+                {
+                    Toast.makeText(LoginActivity.this, "Incorrect Email or Password", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        String success = jsonResponse.getString("success");
+                        if (success == "true") {
+                            Toast.makeText(getApplicationContext(), "Login Successful!", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            else
+            {
+                Toast.makeText(LoginActivity.this, "Please Check Your Internet Connection and Try Again", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+
+
+
+    public String getResponse(HttpURLConnection httpURLConnection)
+    {
+        String result = "";
+        try {
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            int data = inputStreamReader.read();
+
+            while(data!= -1 )
+            {
+                result += (char)data;
+                data = inputStreamReader.read();
+            }
+
+            return result;
+
+               }
+
+        catch(Exception e)
+        {
+            return e.getMessage();
+        }
+    }
+
+
+
+
+
 }
