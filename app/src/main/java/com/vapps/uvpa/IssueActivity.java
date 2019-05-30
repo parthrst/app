7cefe5e36005d7935d19fe8c89d328576d2d1607
Parth <parthrst@gmail.com>
2019-05-30 11:33:12 +0530

    Issue List View Issue resolved

--- app/src/main/java/com/vapps/uvpa/IssueActivity.java
+++ app/src/main/java/com/vapps/uvpa/IssueActivity.java
@@ -1,8 +1,12 @@
 package com.vapps.uvpa;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -12,6 +16,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class IssueActivity extends AppCompatActivity {
    ListView listView;
@@ -45,7 +50,7 @@ public class IssueActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return  3;
+            return  8;
         }
 
         @Override
@@ -60,7 +65,7 @@ public class IssueActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View view, ViewGroup parent) {
-          view=getLayoutInflater().inflate(R.layout.custom_listview,null);
+            view=getLayoutInflater().inflate(R.layout.custom_listview,null);
             CheckBox cb=view.findViewById(R.id.issue);
             TextView es=view.findViewById(R.id.estimate);
             cb.setText(type_repair[position]);
@@ -69,3 +74,4 @@ public class IssueActivity extends AppCompatActivity {
         }
     }
 }
+
